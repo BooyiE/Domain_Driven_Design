b63d829a63999a9503be85c0aad0a2932f7a1382
Boile <boilep@bbd.co.za>
2020-04-19 22:40:14 +0200

    Data

---
+++ Data/Person_data.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    public class Person_data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string ContactNO { get; set; }
+
+        public string Province { get; set; }
+    }
+}
