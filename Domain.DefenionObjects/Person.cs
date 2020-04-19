63aecb4fec86fde9b3a75ac8b4fe8a5fdf0bdbd6
Boile <boilep@bbd.co.za>
2020-04-19 22:45:02 +0200

    Domain.DefenionObjects

---
+++ Domain.DefenionObjects/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DefenionObjects
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public string ContactNO { get; set; }
+
+        public string Province { get; set; }
+    }
+}
