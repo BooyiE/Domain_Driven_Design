9358c472e6ab9975daeec8ecee28706cdf2c84bf
Boile <boilep@bbd.co.za>
2020-04-20 02:28:00 +0200

    Repositories

--- Repositories/PersonRepository.cs
+++ Repositories/PersonRepository.cs
@@ -9,11 +9,54 @@ namespace Repositories
 {
     public class PersonRepository
     {
+            public Person ToDomain(Person_data personData)
+        {
+            return new Person
+            {
+                Id = personData.Id,
+                Name = personData.Name,
+                Surname = personData.Surname,
+                Gender = personData.Gender,
+                ContactNO = personData.ContactNO,
+                Province = personData.Province
+                // set rest of properties.
+            };
+        }
+        public List<Person_data> peoplelist = new List<Person_data>
+        {
+           new Person_data { Id = 1, Name = "Nontobeko", Surname = "Xaba", Gender = "Female", ContactNO = "0783678221", Province = "KwaZulu Natal" },
+            new Person_data { Id = 2, Name = "Agnes", Surname = "Hamilton", Gender = "Female", ContactNO = "0854576571", Province = "Northern cape" },
+            new Person_data { Id = 3, Name = "William", Surname = "Morley", Gender = "Male", ContactNO = "0856893591", Province = "Gauteng" },
+            new Person_data { Id = 4, Name = "Skhumkane", Surname = "Mlotshwa", Gender = "Male", ContactNO = "0776138700", Province = "Free State" },
+        };
+        public void Serialize(List<Person_data> people)
+        {
+            var FilePath = @"C:\Users\bbdnet2223\Desktop\Domain_Driven_Design-ddd-\CRUD_Project_Api\People.xml";
+            TextWriter txtWriter = new StreamWriter(FilePath);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Person_data>));
+
+            serializer.Serialize(txtWriter, people);
+
+            txtWriter.Close();
+        }
+        public List<Person_data> deserializer()
+        {
+            var FilePath = @"C:\Users\bbdnet2223\Desktop\Domain_Driven_Design-ddd-\CRUD_Project_Api\People.xml";
+            List<Person_data> peoples;
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Person_data>));
+            using (Stream reader = new FileStream(FilePath, FileMode.Open))
+            {
+                peoples = (List<Person_data>)serializer.Deserialize(reader);
+
+            }
+            return peoples;
+        }
         public List<Person> GetAll()
         {
             var FilePath = @"C:\Users\bbdnet2223\Desktop\Domain_Driven_Design-ddd-\CRUD_Project_Api\People.xml";
 
-           if (!(File.Exists(FilePath)))
+            if (!(File.Exists(FilePath)))
                 Serialize(peoplelist);
 
             var peopleData = deserializer();//get all the people stored in your xml file
@@ -62,51 +105,6 @@ namespace Repositories
             peopleData.Select(person => ToDomain(person));
         }
 
-
-            public Person ToDomain(Person_data personData)
-        {
-            return new Person
-            {
-                Id = personData.Id,
-                Name = personData.Name,
-                Surname = personData.Surname,
-                Gender = personData.Gender,
-                ContactNO = personData.ContactNO,
-                Province = personData.Province
-                // set rest of properties.
-            };
-        }
-        public List<Person_data> peoplelist = new List<Person_data>
-        {
-           new Person_data { Id = 1, Name = "Nontobeko", Surname = "Xaba", Gender = "Female", ContactNO = "0783678221", Province = "KwaZulu Natal" },
-            new Person_data { Id = 2, Name = "Agnes", Surname = "Hamilton", Gender = "Female", ContactNO = "0854576571", Province = "Northern cape" },
-            new Person_data { Id = 3, Name = "William", Surname = "Morley", Gender = "Male", ContactNO = "0856893591", Province = "Gauteng" },
-            new Person_data { Id = 4, Name = "Skhumkane", Surname = "Mlotshwa", Gender = "Male", ContactNO = "0776138700", Province = "Free State" },
-        };
-        public void Serialize(List<Person_data> people)
-        {
-            var FilePath = @"C:\Users\bbdnet2223\Desktop\Domain_Driven_Design-ddd-\CRUD_Project_Api\People.xml";
-            TextWriter txtWriter = new StreamWriter(FilePath);
-
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Person_data>));
-
-            serializer.Serialize(txtWriter, people);
-
-            txtWriter.Close();
-        }
-        public List<Person_data> deserializer()
-        {
-            var FilePath = @"C:\Users\bbdnet2223\Desktop\Domain_Driven_Design-ddd-\CRUD_Project_Api\People.xml";
-            List<Person_data> peoples;
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Person_data>));
-            using (Stream reader = new FileStream(FilePath, FileMode.Open))
-            {
-                peoples = (List<Person_data>)serializer.Deserialize(reader);
-
-            }
-            return peoples;
-        }
-       
     }
 }
 
